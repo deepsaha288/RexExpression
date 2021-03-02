d526c6853c02c6b336871732db9989afb099259f
Deep Saha <deepsaha288@yahoo.com>
2021-03-02 18:06:31 +0530

    password  regular expression validation check

--- Regax.java
+++ Regax.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class Regax {
 	static Scanner user =new Scanner(System.in);
 
@@ -25,12 +24,7 @@ public class Regax {
 	public static void lastName()
 	{
 		String lastName = user.next();
-		boolean name1= Pattern.matches("^[A-Z]{1}[a-z]{3,}$",lastNamee);
-	}
-	public static void lastName()
-	{
-		String lastName = user.next();
-		boolean name1= Pattern.matches("^[A-Z]{1}[a-z]{3,10}$",lastName);
+		boolean name1= Pattern.matches("^[A-Z]{1}[a-z]{3,}$",lastName);
 		if(name1==true)
 		{
 			System.out.println(lastName+" is valid firstname");
@@ -40,7 +34,9 @@ public class Regax {
 			System.out.println("Please reenter valid user name");
 			lastName();
 		}
+
 	}
+
 	public static void email()
 	{
 		String email = user.next();
@@ -52,33 +48,53 @@ public class Regax {
 		else
 		{ 
 			System.out.println("InValid !!!,,Please reenter valid email");
-			email(); 
+			
 		}
+
+	}
+	public static void phoneNum()
+	{
+		String pnum = user.next(); 
+		boolean num= Pattern.matches("^[0-9]{2}[ ][6-9]{1}[0-9]{9}$",pnum); 
+		if(num==true)
+		{  
+			System.out.println(num+" is valid phone number");
+		} 
+		else
+		{ 
+			System.out.println("Invalid-->Please reenter valid phone number");
+			
+		}
+	}
+	public static void password() 
+	{
+		String pwd= user.next(); 
+		boolean password5= Pattern.matches("^[A-Za-z]{8}$",pwd);
+		if(password5==true)  
+		{ 
+			System.out.println("you have entered valid password");  
+		}
+		else
+		{  
+			System.out.println("Invalid !!-->Please reenter valid password") ;
+			password();
+		}
+
 	}
 
-        public static void phoneNumber() 
-        {
-		String phone = user.nextLine();
-		boolean result = Pattern.matches("^[0-9]{2}[ ][6-9]{1}[0-9]{9}$", phone);
-		if (result == true)
-                {
-			System.out.println("You Have Enter Valid Mobile Number: " +phone);
-		}
-                else
-                {
-			System.out.println("Please Re-enter Mobile Number: ");
-			phoneNumber();
-		}
-	}
+
+
 	public static void main(String[] args) {
-		System.out.println("enter the user First name");
+		System.out.println("enter the user first name");
 		firstName();
 		System.out.println("enter the user last name");
 		lastName();
 		System.out.println("enter the user email id");
 		email();
-                System.out.println("enter the user phone-number ");
-	        phoneNumber();
-    
+		System.out.println("enter the user phone-number ");
+		phoneNum();
+		System.out.println("enter your passward ");
+		password();
+
 	}
 }
