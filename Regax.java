270960dfc4c6fb7f5d8a57351348877a571cd766
Deep Saha <deepsaha288@yahoo.com>
2021-03-02 18:13:12 +0530

    UperCase password  regular expression validation check

--- Regax.java
+++ Regax.java
@@ -1,6 +1,8 @@
+
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 
 public class Regax {
 	static Scanner user =new Scanner(System.in);
@@ -40,7 +42,7 @@ public class Regax {
 	public static void email()
 	{
 		String email = user.next();
-		boolean email1= Pattern.matches("^[A-Za-z0-9-\\\\+]+(\\\\.[A-Za-z0-9-]+)*@\" + \"[A-Za-z0-9-]+(\\\\.[A-Za-z0-9]+)*(\\\\.[A-Za-z]{2,})$",email);
+		boolean email1= Pattern.matches("^[A-Za-z0-9]+(\\.[A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$", email);
 		if(email1==true)
 		{
 			System.out.println(email1+" is valid email");
@@ -48,30 +50,31 @@ public class Regax {
 		else
 		{ 
 			System.out.println("InValid !!!,,Please reenter valid email");
-			
+			email();  
 		}
 
 	}
-	public static void phoneNum()
-	{
-		String pnum = user.next(); 
-		boolean num= Pattern.matches("^[0-9]{2}[ ][6-9]{1}[0-9]{9}$",pnum); 
-		if(num==true)
-		{  
-			System.out.println(num+" is valid phone number");
-		} 
-		else
-		{ 
-			System.out.println("Invalid-->Please reenter valid phone number");
-			
+
+	static void phoneNumber() {
+
+		String phone = user.nextLine();
+		boolean result = Pattern.matches("^[0-9]{2}[ ][6-9]{1}[0-9]{9}$", phone);
+		if (result == true) {
+			System.out.println("You Have Enter Valid Mobile Number: " +phone);
+		}else {
+			System.out.println("Please Re-enter Mobile Number: ");
+			phoneNumber();
 		}
 	}
+
+
 	public static void password() 
 	{
 		String pwd= user.next(); 
-		boolean password5= Pattern.matches("^[A-Za-z]{8}$",pwd);
-		if(password5==true)  
-		{ 
+		boolean passwords= Pattern.matches("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=[^$@!#%*?&]*[$#@!%*?&][^$@!#%*?&]*$).{8,}",pwd);
+
+		if(passwords==true)   
+		{  
 			System.out.println("you have entered valid password");  
 		}
 		else
@@ -85,14 +88,14 @@ public class Regax {
 
 
 	public static void main(String[] args) {
-		System.out.println("enter the user first name");
+		System.out.println("enter the user first name"); 
 		firstName();
 		System.out.println("enter the user last name");
 		lastName();
 		System.out.println("enter the user email id");
 		email();
 		System.out.println("enter the user phone-number ");
-		phoneNum();
+		phoneNumber();
 		System.out.println("enter your passward ");
 		password();
 
